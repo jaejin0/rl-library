53e52cc3a31f6f06f2ecd74d2279212d3da9fbf3
jaejin <jaejin0109@gmail.com>
2024-10-24 15:35:19 -0500

    created Dynamic Programming appraoch with state value function and action value function using Bellman equation

---
+++ dynamic_programming.py
@@ -0,0 +1,138 @@
+from numpy.random import choice 
+from mdp import MDP
+
+class DynamicProgramming:
+    # n-tuple (S, A, R, T, gamma) 
+    def __init__(self, state_space, action_space, reward_function, transition_function, discount_factor, iteration_limit):
+        self.state_space = state_space # 1-D array of state space
+        self.action_space = action_space # 1-D array of action space
+        self.reward_function = reward_function # function(state, action, next_state) -> float 
+        self.transition_function = transition_function # function(state, action, next_state) -> float
+        self.discount_factor = discount_factor # <= 1 (float)
+        self.iteration_limit = iteration_limit # depth of bellman equaition (int)
+
+    def state_value_policy(self, state) -> float:
+        max_action, max_value = choice(action_space), 0
+        for a in action_space:
+            sum_over_state_space = 0
+            for next_s in self.state_space:
+                sum_over_state_space += self.transition_function(state, a, next_s) * (reward_function(state, a, next_s) + (self.discount_factor * self.state_value_function(next_s, self.iteration_limit - 1)))
+            
+            if sum_over_state_space > max_value:
+                max_value = sum_over_state_space
+                max_action = a
+
+        return max_action
+
+    def action_value_policy(self, state) -> float:
+        max_action, max_value = choice(action_space), 0
+        for a in action_space:
+            action_value = self.action_value_function(state, a)
+            if action_value > max_value:
+                max_value = action_value
+                max_action = a
+        
+        return max_action
+
+    def state_value_function(self, state, iteration_limit = None) -> float:        
+        iteration_limit = self.iteration_limit if iteration_limit == None else iteration_limit
+        if iteration_limit <= 0:
+            return 0
+        sum_over_action_space = 0
+        for a in self.action_space:
+            sum_over_state_space = 0
+            for next_s in self.state_space:
+                sum_over_state_space += self.transition_function(state, a, next_s) * (reward_function(state, a, next_s) + (self.discount_factor * self.state_value_function(next_s, iteration_limit - 1)))
+            sum_over_action_space += sum_over_state_space
+        
+        return sum_over_action_space
+
+    def action_value_function(self, state, action, iteration_limit = None) -> float:
+        iteration_limit = self.iteration_limit if iteration_limit == None else iteration_limit
+        if iteration_limit <= 0:
+            return 0
+        sum_over_state_space = 0
+        for next_s in self.state_space:
+            sum_over_action_space = 0
+            for next_a in self.action_space:
+                sum_over_action_space += self.discount_factor * self.action_value_function(next_s, next_a, iteration_limit - 1)
+            sum_over_state_space += self.transition_function(state, action, next_s) * (reward_function(state, action, next_s) + sum_over_action_space)
+        
+        return sum_over_state_space
+
+
+if __name__ == "__main__":
+    
+    # MDP specification
+    grid_height = 3
+    grid_width = 4
+
+    state_space = []
+    for i in range(grid_height):
+        for j in range(grid_width):
+            state_space.append((i, j))
+
+    initial_state_distribution = []
+    for i in range(grid_height):
+        for j in range(grid_width):
+            initial_state_distribution.append(0)
+
+    initial_state_distribution[0] = 0.6
+    initial_state_distribution[1] = 0.2
+    initial_state_distribution[grid_width] = 0.2
+
+    action_space = ['left', 'right', 'up', 'down', 'no-op'] 
+
+    def reward_function(state, action, next_state):
+        if next_state == (grid_height - 1, grid_width - 1): # goal
+            return 1
+        else:
+            return 0
+
+    def transition_function(state, action, next_state):
+        match action:
+            case 'left':
+                if state[1] == 0 and state == next_state: # can't move to the left further
+                    return 1
+                if state[1] != 0 and state[1] - 1 == next_state[1] and state[0] == next_state[0]:
+                    return 1
+            case 'right':
+                if state[1] == grid_width - 1 and state == next_state: # can't move to the right further
+                    return 1
+                if state[1] != grid_width - 1 and state[1] + 1 == next_state[1] and state[0] == next_state[0]:
+                    return 1
+            case 'up':
+                if state[0] == 0 and state == next_state: # can't move up further
+                   return 1
+                if state[0] != 0 and state[0] - 1 == next_state[0] and state[1] == next_state[1]:
+                   return 1
+            case 'down':
+                if state[0] == grid_height - 1 and state == next_state: # can't move down further
+                    return 1
+                if state[0] != grid_height - 1 and state[0] + 1 == next_state[0] and state[1] == next_state[1]:
+                    return 1
+        return 0
+    print(state_space, initial_state_distribution) 
+    mdp = MDP(state_space, action_space, reward_function, transition_function, initial_state_distribution)
+    print(mdp.current_state)
+
+    discount_factor = 0.5
+    iteration_limit = 4
+    policy = DynamicProgramming(state_space, action_space, reward_function, transition_function, discount_factor, iteration_limit)
+
+    while True:
+        current_state = mdp.current_state
+        current_reward = mdp.current_reward
+        print(f"Current state is  : {current_state}")
+        print(f"Current reward is : {current_reward}")
+
+        print(f"Current state value is : {policy.state_value_function(current_state)}")
+        action = policy.state_value_policy(current_state)
+        print(f"Action chosen by state_value_policy agent is : {action}")
+        
+        print(f"Current action value is : {policy.action_value_function(current_state, action)}")
+        action = policy.action_value_policy(current_state)
+        print(f"Action chosen by action_value_policy agent is : {action}")
+
+        foo = input(f"press any key to forward")
+        mdp.forward(action)
--- mdp.py
+++ mdp.py
@@ -3,21 +3,24 @@ from numpy.random import choice
 class MDP:
     # 5-tuple (S, A, R, T, mu)
     def __init__(self, state_space, action_space, reward_function, transition_function, initial_state_distribution):
-        self.state_space = state_space # [s1, s2, ... ]
-        self.action_space = action_space # [a1, a2, ... ]
+        self.state_space = state_space # 1-D array of state space [s1, s2, ... ]
+        self.action_space = action_space # 1-D array of action space [a1, a2, ... ]
         self.reward_function = reward_function # function(state, action, next_action) -> float
         self.transition_function = transition_function # function(state, action, next_action) -> float
-        self.initial_state_distribution = initial_state_distribution # [pr1, pr2, ... ] # same size as the state_space, probabilties sums up to 1. 
-
-    def start(self):
-        self.current_state = choice(self.state_space, p=self.initial_state_distribution)
+        self.initial_state_distribution = initial_state_distribution # 1-D array of probability of state space [pr1, pr2, ... ] # same size as the state_space, probabilties sums up to 1. 
+        
+        self.reset()
+    
+    def reset(self):
+        state_index = choice(len(self.state_space), 1, p=self.initial_state_distribution)[0]
+        self.current_state = self.state_space[state_index]
+        self.current_reward = 0 # at t = 0, reward is 0
 
     def forward(self, action):
-        # update position of state with the value of action
-        next_state = choice(self.state_space, p=self.probability_of_next_state(self.current_state, action))
-        reward = self.reward_function(self.current_state, action, next_state)
+        next_state_index = choice(len(self.state_space), 1, p=self.probability_of_next_state(self.current_state, action))[0]
+        next_state = self.state_space[next_state_index]
+        self.current_reward = self.reward_function(self.current_state, action, next_state)
         self.current_state = next_state
-        return reward
 
     def probability_of_next_state(self, state, action):
         probability_of_next_state = []
@@ -55,13 +58,13 @@ def transition_function(state, action, next_state) -> float:
 
 if __name__ == "__main__":
 
+    # 1-D game
     state_space = [0, 1, 2, 3]
     action_space = ["left", "no-op", "right"]
     initial_state_distribution = [0, 0.2, 0.5, 0.3]
 
     mdp = MDP(state_space, action_space, reward_function, transition_function, initial_state_distribution)
 
-    mdp.start()
     while True:
         print(f"You are at {mdp.current_state}")
         print(f"Choose from the following: {action_space}")
@@ -74,4 +77,4 @@ if __name__ == "__main__":
             case _:
                 action = "no-op"
         mdp.forward(action)
-        print(f"reward: {mdp.forward(user_input)}")
+        print(f"reward: {mdp.current_reward}")
