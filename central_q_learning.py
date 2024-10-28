5d69be815234820741a21fba03d35a2df262b89b
jaejin <jaejin0109@gmail.com>
2024-10-27 21:54:43 -0500

    finished implementing central q learning

--- central_q_learning.py
+++ central_q_learning.py
@@ -2,6 +2,7 @@ import torch
 from vmas import make_env
 from vmas.simulator.heuristic_policy import BaseHeuristicPolicy, RandomPolicy
 import numpy as np
+from itertools import combinations
 
 # Temporal-Difference (TD) Learning is a family of RL algorithm that learn optimal policies and value functions based on data collected via environment interations.
 # For an example of TD Learning, V(s^t) <- V(s^t) + alpha(X - V(s^t)), where alpha is learning rate and X is the update target.
@@ -27,36 +28,28 @@ class CentralQLearning():
         self.discrete_observation_space = [i/10 for i in range(OBS_RANGE[0], OBS_RANGE[1], OBS_STEP)]
         self.discrete_action_space = [i/10 for i in range(ACTION_RANGE[0], ACTION_RANGE[1], ACTION_STEP)]
         
-        print(self.joint_action_space)
-        print(len(self.joint_observation_space) * self.joint_observation_space[0].shape[0])
-
-        print(len(self.joint_action_space) * self.joint_action_space[0].shape[0])
         self.joint_action_value_table = {} # key : Tuple(Tuple(discrete_joint_observation), Tuple(discrete_joint_action))
-        # self.action_value_table = np.zeros([len(self.observation_space) * self.observation_space[0].shape[0], 
-        #                                     len(self.action_space) * self.action_space[0].shape[0]])
+        
         self.prev_joint_observation = None
         self.prev_joint_action = None
 
     def policy(self, joint_observation):
-        # discretize state
+        # discretize the joint observation and store as a Tuple
+        discrete_joint_observation = ()
+        for obs in joint_observation:
+            discrete_observation = []
+            for o in obs[0]:
+                discrete_observation.append(min(self.discrete_observation_space, key=lambda x: abs(x - o)))
+            discrete_joint_observation += tuple(discrete_observation)
         
         # explore
-        joint_action = []
+        discrete_joint_action = []
         for i in range(self.n_agents):
-            print("here is", self.joint_action_space[0].shape[0], self.joint_action_space[0].low, self.joint_action_space[0].high[0])
-            joint_action.append(torch.clamp(torch.randn(env.num_envs, self.joint_action_space[0].shape[0]), 
-                                                self.joint_action_space[0].low[0], self.joint_action_space[0].high[0]))
-
-        
+            discrete_joint_action.append(torch.from_numpy(np.expand_dims(np.random.choice(self.discrete_action_space, self.joint_action_space[0].shape[0]), 0)))
+        discrete_joint_action = tuple(discrete_joint_action)
+         
         choose_random_joint_action = np.random.binomial(1, self.exploration_parameter)
-        if not choose_random_joint_action: # exploit
-            discrete_joint_observation = ()
-            for obs in joint_observation:
-                discrete_observation = []
-                for o in obs[0]:
-                    discrete_observation.append(min(self.discrete_observation_space, key=lambda x: abs(x - o)))
-                discrete_joint_observation += tuple(discrete_observation)
-            
+        if not choose_random_joint_action: # exploit based on exploration parameter
             max_val = 0
             for k in self.joint_action_value_table:
                 if k[0] != discrete_joint_observation:
@@ -64,23 +57,34 @@ class CentralQLearning():
 
                 if self.joint_action_value_table[k] > max_val:
                     max_val = self.joint_action_value_table[k]
-                    joint_action = k[1]
-                    print("The exploit action is : ", joint_action)
+                    discrete_joint_action = k[1]
 
-        self.prev_joint_observation = joint_observation
-        self.prev_joint_action = joint_action
+        self.prev_discrete_joint_observation = discrete_joint_observation
+        self.prev_discrete_joint_action = discrete_joint_action
 
-        return joint_action
+        return discrete_joint_action
 
-    def learn_action_value_function(self, observation, reward):
-        max_value = 0
-        for a in action_space:
-            max_value = max(max_value, self.action_value_table[self.state_space.index(state)][self.action_space.index(a)])
-        self.action_value_table[self.state_space.index(self.prev_state)][self.action_space.index(self.prev_action)] += \
-            self.learning_rate * (reward + (self.discount_factor * max_value) -  
-                                  self.action_value_table[self.state_space.index(self.prev_state)][self.action_space.index(self.prev_action)])
+    def learn_joint_action_value_function(self, joint_observation, joint_reward):
+        # finding tuple of discretized joint observation
+        discrete_joint_observation = ()
+        for obs in joint_observation:
+            discrete_observation = []
+            for o in obs[0]:
+                discrete_observation.append(min(self.discrete_observation_space, key=lambda x: abs(x - o)))
+            discrete_joint_observation += tuple(discrete_observation)
+        
+        max_value = 0 
+        for next_discrete_joint_a in combinations(self.discrete_action_space, self.n_agents):
+            # search how to get certain number of every combination of a list
+            
+            max_value = max(max_value, self.joint_action_value_table.get((discrete_joint_observation, next_discrete_joint_a), 0))
 
-   
+        self.joint_action_value_table[(self.prev_discrete_joint_observation, self.prev_discrete_joint_action)] = \
+                self.joint_action_value_table.get((self.prev_discrete_joint_observation, self.prev_discrete_joint_action), 0) + \
+                self.learning_rate * (joint_reward + (self.discount_factor * max_value) - 
+                                      self.joint_action_value_table.get((self.prev_discrete_joint_observation, self.prev_discrete_joint_action), 0))
+  
+
 if __name__ == "__main__":
     
     env = make_env(
@@ -101,28 +105,28 @@ if __name__ == "__main__":
     learning_rate = 0.1
     exploration_parameter = 0.3
 
-    print(env.get_observation_space) 
     agent = CentralQLearning(env.observation_space, env.action_space, discount_factor, learning_rate, exploration_parameter) 
+    print(env.max_steps)
+    episode = 100
+    for ep in range(episode):
+        print(f"EPISODE: {ep}")
+        joint_observation = env.reset()
+        for _ in range(env.max_steps):
+            joint_action = agent.policy(joint_observation)
+            print(f"This is the observation\n: {joint_observation}")
+            print(f"This is the action\n: {joint_action}")
+            joint_observation, joint_reward, dones, info = env.step(joint_action)
+            print(f"This is reward: {joint_reward}")
+            print(f"This is dones: {dones}")
+            print(f"This is info: {info}")
+            total_reward = torch.stack(joint_reward, dim=1).mean(dim=1).mean(dim=0)[0].item()
+            print(f"total reward: {total_reward}")
+            agent.learn_joint_action_value_function(joint_observation, total_reward)
 
-    policy = RandomPolicy(continuous_action=True)
-    obs = env.reset()
-
-    for _ in range(1000):
-#        actions = [None] * len(obs)
- #       for i in range(len(obs)):
-  #          actions[i] = policy.compute_action(obs[i], u_range=env.agents[i].u_range)
-        actions = agent.policy(obs)
-        print(f"This is the action\n: {actions}")
-        print(f"This is the observation\n: {obs}")
-        obs, joint_reward, dones, info = env.step(actions)
-        print(f"This is reward: {joint_reward}")
-        print(f"This is dones: {dones}")
-        print(f"This is info: {info}")
-        # rewards = torch.stack(rews, dim=1)
-        # global_reward = rewards.mean(dim=1)
-        # mean_global_reward = global_reward.mean(dim=0)
-        env.render(
-            mode="human",
-            agent_index_focus=None,
-        )
-        input("press any key to continue")
+            if dones[0].item() == True:
+                joint_observation = env.reset()
+            env.render(
+                mode="human",
+                agent_index_focus=None,
+            )
+            # input("press any key to continue")
