5b57ceb2008bd26c6399e1fbf3e542af2e0a24b7
jaejin <jaejin0109@gmail.com>
2024-10-31 03:50:58 -0500

    in process of developing list of action value functions

--- minimax_q.py
+++ minimax_q.py
@@ -23,33 +23,43 @@ import numpy as np
 #         Q_j(s^t, a^t) <- Q_j(s^t, a^t) + alpha[r^t_j + gamma * Value_j(Gamma_{s^{t+1}}) - Q_j(s^t, a^t)]
 
 class MinimaxQ:
-    def __init__(self, state_space, action_space, discount_factor, learning_rate, exploration_parameter):
+    # 5 inputs: 
+    def __init__(self, num_agents, state_space, action_space, discount_factor, learning_rate, exploration_parameter):
+        self.num_agents = num_agents
         self.state_space = state_space
         self.action_space = action_space
         self.discount_factor = discount_factor
         self.learning_rate = learning_rate
         self.exploration_parameter = exploration_parameter
 
-        self.n_agents = len(state_space)
         # defining normal form game for each state
-        self.action_value_function = [np.zeros([len(state_space), len(action_space)]) for i in range(self.n_agents)]
-        self.action_value_table = np.zeros([len(state_space), len(action_space)])
+        self.action_value_function = [np.zeros([len(state_space), len(action_space)]) for i in range(self.num_agents)]
          
 
-
 if __name__ == '__main__':
+
+    # configuration:
+    num_agents = 2
+    discount_factor = 0.99
+    learning_rate = 0.1
+    exploration_parameter = 0.2 
+
     # env.observation_space(agent) = Box(0, 255, (210, 160, 3), uint8)
     # env.action_space(agent) = Discrete(6)
     env = basketball_pong_v3.env(render_mode="human")
     env.reset()
+    print(env.agent_iter()) 
+    # agents = MinimaxQ(num_agents, )
+
     for agent in env.agent_iter():
         observation, reward, termination, truncation, info = env.last()
+        print(observation)
         if termination or truncation:
             action = None
         else:
             # this is where you would insert your policy
             action = env.action_space(agent).sample()
-
+        input()
         env.step(action)
     env.close()
 
