4dbe7e1d5612ee448f1e670bb4510bcb9ba1e54b
jaejin <jaejin0109@gmail.com>
2024-11-09 23:50:58 -0600

    fixed different naming convention of observation in LBF foraging env program, and added info variable for env.reset() function

--- lbf_mdp.py
+++ lbf_mdp.py
@@ -4,7 +4,7 @@ import time
 
 # Level-based Foraging (LBF): a multi-agent environment for RL
 env = gym.make("Foraging-8x8-2p-1f-v3")
-obs = env.reset()
+observations, info = env.reset()
 
 max_steps = 1000
 for _ in range(max_steps):
