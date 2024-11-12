54456dda7355a310b96f6d76cbed961cd98cf0a3
jaejin <jaejin0109@gmail.com>
2024-11-12 16:27:33 -0600

    implemented Generator class in GAIL

--- gail.py
+++ gail.py
@@ -1,14 +1,40 @@
 # GAIL : Generative Adversarial Imitation Learning
 
-class Generator():
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.distributions import Normal
 
-    def __init__():
+class Generator(nn.Module):
+    def __init__(self, state_dim, action_dim, hidden_dim=256):
+        super(Generator, self).__init__()
 
+        self.network = nn.Sequential(
+            nn.Linear(state_dim, hidden_dim),
+            nn.ReLU(),
+            nn.Linear(hidden_dim, hidden_dim),
+            nn.ReLU()
+        )
 
-    def policy(self, random_input):
-        pass
-        
-        return sample
+        # Mean and standard deviation for continious actions
+        self.mean = nn.Linear(hidden_dim, action_dim)
+        self.log_std = nn.Linear(hidden_dim, action_dim)
+
+    def forward(self, state):
+        x = self.network(state)
+        mean = self.mean(x)
+        log_std = self.log_std(x)
+        std = torch.exp(log_std)
+        return mean, std
+
+class Discriminator(nn.Module):
+    def __init__(self, state_dim, action_dim, hidden_dim=256):
+        super(Discrminator, self).__init__()
+
+        self.network = nn.Sequential(
+        )
+
 
 class Discriminator():
     '''
